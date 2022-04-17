9419849adfb9c1e1677a5967b194af27bc01fbe2
shp <stevenhalampaek@gmail.com>
2022-04-17 18:22:21 -0500

    Adding MIPS top level

--- rtl/mips_top.v
+++ rtl/mips_top.v
@@ -9,33 +9,72 @@
                     /* Inputs */
                     input port_sel,
                     input port_en,
-                    input [7:0] switches,
+                    input [31:0] user_input,
                     /* Outputs */
                     output [15:0] leds);
 
     /* Internal Signals */
-    wire [31:0] inst_opcode;
-    wire [5:0]  alu_opcode;
-    wire        pc_write_cond;
-    wire        pc_write;
-    wire        i_or_d;
-    wire        mem_read;
-    wire        mem_write;
-    wire        mem_to_reg;
-    wire        ir_write;
-    wire        is_signed;
-    wire [1:0]  pc_source;
-    wire        alu_src_a;
-    wire [1:0]  alu_src_b;
-    wire        reg_write;
-    wire        reg_dest;
-    wire        jump_and_link;
+    wire [31:0] opcode;
+    wire [5:0]  ALUOp;
+    wire        PCWriteCond;
+    wire        PCWrite;
+    wire        IorD;
+    wire        MemRead;
+    wire        MemWrite;
+    wire        MemToReg;
+    wire        IRWrite;
+    wire        isSigned;
+    wire [1:0]  PCSource;
+    wire        ALUSrcA;
+    wire [1:0]  ALUSrcB;
+    wire        RegWrite;
+    wire        RegDst;
+    wire        JumpAndLink;
     wire [31:0] outport;
     wire        port_rst;
 
     /* Instantiate Sub Blocks */
-    mips_controller i_mips_controller (.clk(clk), .rst(rst));
+    mips_controller i_mips_controller ( .clk(clk), 
+                                        .rst(rst),
+                                        .opcode(opcode),
+                                        .PCWriteCond(PCWriteCond),
+                                        .PCWrite(PCWrite),
+                                        .IorD(IorD),
+                                        .MemRead(MemRead),
+                                        .MemWrite(MemWrite),
+                                        .MemToReg(MemToReg),
+                                        .IRWrite(IRWrite),
+                                        .isSigned(isSigned),
+                                        .PCSource(PCSource),
+                                        .ALUOp(ALUOp),
+                                        .ALUSrcA(ALUSrcA),
+                                        .ALUSrcB(ALUSrcB),
+                                        .RegWrite(RegWrite),
+                                        .RegDst(RegDst),
+                                        .JumpAndLink(JumpAndLink));
 
-    mips_datapath i_mips_datapath (.clk(clk), .rst(rst));
+    mips_datapath i_mips_datapath ( .clk(clk), 
+                                    .rst(rst),
+                                    .port_rst(port_rst),
+                                    .PCWriteCond(PCWriteCond),
+                                    .PCWrite(PCWrite),
+                                    .IorD(IorD),
+                                    .MemRead(MemRead),
+                                    .MemWrite(MemWrite),
+                                    .MemToReg(MemToReg),
+                                    .IRWrite(IRWrite),
+                                    .JumpAndLink(JumpAndLink),
+                                    .isSigned(isSigned),
+                                    .PCSource(PCSource),
+                                    .ALUOp(ALUOp),
+                                    .ALUSrcA(ALUSrcA),
+                                    .ALUSrcB(ALUSrcB),
+                                    .RegWrite(RegWrite),
+                                    .RegDst(RegDst),
+                                    .in_sel(in_sel),
+                                    .in_en(in_en),
+                                    .in0_1(user_input),
+                                    .outport(outport),
+                                    .opcode(opcode));
 
  endmodule
